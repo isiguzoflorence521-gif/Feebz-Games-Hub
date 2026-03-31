7f0fd3810c021aaac4783d274705df2291ff35d5
IfeanyiPlayzz <isiguzoflorence521@gmail.com>
2026-03-31 15:08:17 -0400

    Create s24_optimize.sh

---
+++ automation/s24_optimize.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Force QHD+ Resolution
+wm size 1440x3120
+# Force 120Hz Refresh Rate
+settings put system peak_refresh_rate 120.0
+settings put system min_refresh_rate 120.0
+echo "S24 Ultra Optimized for Feebz VR"
